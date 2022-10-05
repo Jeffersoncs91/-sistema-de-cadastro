b74b51ca068c57d395cbc1b412a61b5e392ebbe0
jeffersoncs91 <jeffersoncs91@gmail>
2022-10-05 14:19:44 -0300

    criando teste

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectado ao banco de dados")
-<<<<<<< HEAD
 console.log("teste2")
-=======
-console.log("teste")
->>>>>>> 58fa093c94982e1639a8f8f4f2e8091481edf948
